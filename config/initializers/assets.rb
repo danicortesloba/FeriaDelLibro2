dea906f7997712fd966838cbed6de5bbe8f519c8
Dani <dustyglass@gmail.com>
2019-01-06 15:10:01 -0300

    ADD: template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,24 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('assets', 'images', 'fallback')
+Rails.application.config.assets.paths << Rails.root.join('vendor')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'audio')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'author')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'banner')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'blog')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'cart')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'flag')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'flex')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'icon-img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'logo')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'post')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'product')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'slider')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'team')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'thum-2')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js', 'vendor')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
