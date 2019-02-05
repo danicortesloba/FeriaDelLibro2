79a26d14290a95b8c90f2758445df02c8da8f2d2
Dani <dustyglass@gmail.com>
2019-02-05 20:15:37 -0300

    add sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-before_action :set_raven_context
+
 
 rescue_from CanCan::AccessDenied do |exception|
      respond_to do |format|
@@ -7,9 +7,4 @@ rescue_from CanCan::AccessDenied do |exception|
      end
  end
 
-
-  def set_raven_context
-    Raven.user_context(id: session[:current_user_id]) # or anything else in session
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
 end
