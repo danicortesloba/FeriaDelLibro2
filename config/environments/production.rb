3e4f3c7560e4b209c79b174875bc6ceed4b9c4d6
Dani <dustyglass@gmail.com>
2019-03-04 21:50:09 -0300

    static files to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
   if ENV["RAILS_LOG_TO_STDOUT"].present?
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
@@ -34,7 +34,7 @@ end
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.serve_static_assets = true
-  
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
